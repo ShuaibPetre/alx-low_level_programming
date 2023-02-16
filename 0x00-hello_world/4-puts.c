ed5957d56db830d690a015606d133ec864f6e527
Shuaib Petre <shuaib.petre@gmail.com>
2023-02-16 09:40:01 -0800

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+* main -Entry point
+*
+* puts - prints string
+* Return: always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+	return (0);
 }
