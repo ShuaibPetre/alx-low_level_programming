8b7e7bc0ef464a5dc02d7a847da9397701f60ece
Shuaib Petre <shuaib.petre@gmail.com>
2023-02-17 05:52:43 -0800

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1 always success
+ */
+
+int main(void)
+{
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19i\n";
+
+	write(2, c, 59);
+	return (1);
+}
