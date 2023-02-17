8789f4b5ad56ae7dff1c3c3ff811006ffc033f23
Shuaib Petre <shuaib.petre@gmail.com>
2023-02-17 05:53:32 -0800

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19i\n";
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, c, 59);
 	return (1);
