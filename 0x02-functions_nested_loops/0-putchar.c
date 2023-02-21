2f96698bc239d30eb2f962d89b1c80f73c7c205f
Shuaib Petre <shuaib.petre@gmail.com>
2023-02-21 09:14:49 -0800

    s

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * _putchar writes _putchar to stdout
+ * _putchar the string to print
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
