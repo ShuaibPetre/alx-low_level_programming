2acca14f2cbac7b5779c3c4c89e58d2ac3c6c6fc
Shuaib Petre <shuaib.petre@gmail.com>
2023-02-22 09:41:48 -0800

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * main - print putchar
- * Return: always 0
+ * main - print _putchar
+ * Return: Always 0 (successul)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -15,8 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-
+	_putchar('n');
 	return (0);
-
 }
